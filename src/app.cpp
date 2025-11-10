4d67c4a7140be9639ac7f9d056fdc76489576152
TamKungZ_ <kittiwut.pimpromma@gmail.com>
2025-11-10 17:01:42 +0700

    Create app.cpp

---
+++ src/app.cpp
@@ -0,0 +1,22 @@
+#include "vliva/app.hpp"
+#include <iostream>
+
+namespace vliva {
+
+App::App() {
+    std::cout << "[VLiva] App constructed (basic)\n";
+}
+
+void App::init() {
+    // (Basic version: Not implemented)
+}
+
+void App::loadModel(const std::string& path) {
+    // (Basic version: Not implemented)
+}
+
+void App::run() {
+    // (Basic version: Not implemented)
+}
+
+} // namespace vliva
